c6edbed47f8495ff49a9be55fd0c3faf9b0710ee
Hayden <hayden.hapgood@my.jcu.edu.au>
2024-09-29 16:15:38 +1000

    Made score menu

---
+++ prac_02/score_menu.py
@@ -0,0 +1,39 @@
+from score import check_score
+
+def main():
+    score = None
+    choice = ""
+
+    while choice != "Q":
+        print("(G)et score")
+        print("(P)rint result")
+        print("(S)how stars")
+        print("(Q)uit")
+        choice = input(">>> ")
+
+        if choice == "G":
+                score = int(input('Enter score (0-100): '))
+                if score < 0 or score > 100:
+                    print('Invalid score.')
+                else:
+                    print('Your score has been saved.')
+
+        elif choice == "P":
+            if score != None:
+                result = check_score(score)
+                print('Your score is:', result)
+            else:
+                print('No score saved.')
+
+        elif choice == "S":
+            if score != None:
+                print('*' * score)
+            else:
+                print('No score saved.')
+
+        elif choice == "Q":
+            print("Finished")
+
+        else:
+            print("Invalid input")
+main()
