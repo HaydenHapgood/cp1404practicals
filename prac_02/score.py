deb83600ec114546d9cde1521c61cdeb6cfaa21c
Hayden <hayden.hapgood@my.jcu.edu.au>
2024-09-29 16:15:28 +1000

    Updated score to put check score function at top and use name and main things to only execute main function when executing score itself

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,3 +1,14 @@
+
+def check_score(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
 import random
 
 def main():
@@ -10,14 +21,7 @@ def main():
     random_result = check_score(random_score)
     print(random_result)
 
-def check_score(score):
-    if score < 0 or score > 100:
-        return "Invalid score"
-    elif score >= 90:
-        return "Excellent"
-    elif score >= 50:
-        return "Passable"
-    else:
-        return "Bad"
-
-main()
+# used ai to help get this part, looked over very briefly in the lectures.
+# causes the main function to only execute when this file is executed, allowing the use of check_score in menu
+if __name__ == "__main__":
+    main()
