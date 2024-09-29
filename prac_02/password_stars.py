b0842050a2083620484a0b4c65b9d911d16c2303
Hayden <hayden.hapgood@my.jcu.edu.au>
2024-09-29 14:12:26 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,3 +1,22 @@
-password = input('Enter password: ')
-for i in range(len(password)):
-    print('*', end=" ")
+minlength = 5
+password = ""
+
+
+def get_password():
+    global password
+    while len(password) < minlength:
+        password = input('Enter password: ')
+        if len(password) < minlength:
+            print('Password is too short, please try again.')
+
+
+get_password()
+
+
+def print_password():
+    for i in range(len(password)):
+        print('*', end=" ")
+    print()
+
+
+print_password()
