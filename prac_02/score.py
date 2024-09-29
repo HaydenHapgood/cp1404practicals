6bbff5660122b036ca6f3b451be064f74806f050
Hayden <hayden.hapgood@my.jcu.edu.au>
2024-09-29 14:36:56 +1000

    Copied broken score into score

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,9 +1,23 @@
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90 and score <= 100:
-    print("Excellent")
-elif score >= 50 and score < 90:
-    print("Passable")
-else:
-    print("Bad")
+import random
+
+def main():
+    score = float(input("Enter score: "))
+    result = check_score(score)
+    print(result)
+
+    random_score = random.randint(1, 100)
+    print('Random score:', random_score)
+    random_result = check_score(random_score)
+    print(random_result)
+
+def check_score(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+main()
