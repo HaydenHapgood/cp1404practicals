019334a4f4aa8af95deb01be7c085fbdd18ee014
Hayden <hayden.hapgood@my.jcu.edu.au>
2024-10-06 17:30:31 +1000

    Added answers

--- prac_03/files.py
+++ prac_03/files.py
@@ -8,3 +8,15 @@ read_name = name_file.read().strip()
 name_file.close()
 print(f'Hi {read_name}!')
 
+total_number = 0
+with open('numbers.txt', 'r') as numbers_file:
+    for i in range(2):
+        number = numbers_file.readline().strip()
+        total_number += int(number)
+    print(total_number)
+
+total_sum = 0
+with open('numbers.txt', 'r') as numbers_file:
+    for line in numbers_file:
+        total_sum += int(line)
+    print(total_sum)
