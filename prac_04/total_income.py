d56baa4103e4dc15ee74d6a26173775254d671b7
Hayden <hayden.hapgood@my.jcu.edu.au>
2024-10-13 16:35:27 +1000

    Refactored report function to use enumerate.

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -15,13 +15,12 @@ def main():
 
     return incomes, number_of_months
 
-def generate_report(incomes, number_of_months):
+def generate_report():
     print("\nIncome Report\n-------------")
     total = 0
-    for month in range(1, number_of_months + 1):
-        income = incomes[month - 1]
+    for month, income in enumerate(incomes, start=1):
         total += income
         print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
 
 incomes, number_of_months = main()
-generate_report(incomes, number_of_months)
+generate_report()
