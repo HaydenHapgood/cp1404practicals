0673f6a8f3f241f5fba707415b56041dce93fed1
HaydenHapgood <hayden.hapgood@my.jcu.edu.au>
2024-11-22 23:24:33 +1000

    Completed silver service taxi exercise.

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -1,5 +1,5 @@
 
-from car import Car
+
 from taxi import Taxi
 
 my_taxi = Taxi("Prius 1", 100)
@@ -7,6 +7,4 @@ my_taxi.drive(40)
 print(my_taxi)
 print(f"Current fare: ${my_taxi.get_fare():.2f}")
 my_taxi.start_fare()
-my_taxi.drive(100)
-print(my_taxi)
-print(f"Current fare: ${my_taxi.get_fare():.2f}")
+
